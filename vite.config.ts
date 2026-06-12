edd656cc9a07ea1787d49aef744969f3da233bb5
Sidharth <sidhartharvind.1808@gmail.com>
2026-06-12 20:52:12 +0530

    done

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Orbit_Sentinel_Dev/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
